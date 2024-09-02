9425a3ffb0b377e6d9a35fbef0b77f218ed338a6
Gracy Gulati <124993594+gracygulati7@users.noreply.github.com>
2024-09-02 11:47:31 +0530

    Delete

--- backend/routes/cars.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getCars, getCar } = require("../controllers/carController");
-
-router.get("/getCars", getCars);
-router.get("/getCar/:id",getCar)
-
-module.exports = router;
